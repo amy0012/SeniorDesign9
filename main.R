1cdd6735586d95a72def9fa696e18c8f547fd434
amy0012 <35613028+amy0012@users.noreply.github.com>
2019-02-03 18:07:23 -0600

    Update main.R

--- main.R
+++ main.R
@@ -1,26 +1,157 @@
-library(shiny)
+#
+# This is a Shiny web application. 
+# 1st Prototype for the Biology Tool Kit
+# 2/3/2019
+#
 
-ui <- fluidPage(
-  #use shiny js to disable the ID field
-  shinyjs::useShinyjs(),
-  #data table
-  DT::dataTableOutput("responses", width = 300), 
-  #input fields
-  tags$hr(),
-  shinyjs::disabled(textInput("id", "Id", "0")),
-  textInput("chrom", "Chromosome", ""),
-  textInput("chromStart", "ChromStart", ""),
-  textInput("chromEnd", "ChromEnd", ""),
-  textInput("name", "Name", ""),
-  textInput("score", "Score", ""),
-  textInput("blocks", "Blocks", ""),
-  #action buttons
-  actionButton("submit", "Submit"),
-  actionButton("new", "New"),
-  actionButton("delete", "Delete")
+library(shiny)
+library(shinythemes)
+library(DT)
+
+data <- df
+names(data) <- c('chrom', 'start', 'stop', 'name', 'score', 'strand')
+
+ui <- navbarPage(
+  
+  "Computational Biology Bio-Informatics Toolkit",
+ theme = shinytheme("cosmo"),
+
+  #---page tab settings---#
+  tabsetPanel(type = "tab", position = "right",
+              
+              #SEARCH TAB
+              tabPanel(title = "Search", 
+                       sidebarPanel(
+                         
+                         condition = "input$id == 'Search'",
+                         helpText("Welcome to the Biology Bio-Information ToolKit!
+                                  Use the options below to search."),
+                         br(),
+                         
+                         #---user input for search---#
+                         # textInput("option1", "select option1"),
+                         # textInput("option2", "select option2"),
+                         
+                         selectInput("dataset", "Select a disease type: ", c("", "type 1", "type 2", "type 3")),
+                         selectInput("option2", "Select a tissue and cell line: ", c("", "cell line 1", "cell line 2", "cell line 3", "cell line 4")),
+                         
+                         helpText("Select your download format."),
+                         radioButtons("type", "Format Type: ", 
+                                      choices = c("Excel (CSV)", "Text(TSV)", "Text(Space Separated)", "Doc")
+                         ),
+                         br(),
+                         
+                         helpText("Click the download button to download your results."),
+                         downloadButton('downloadData', 'Download')
+                         
+                         ),
+                       mainPanel(
+                         h3("Search Results: "),
+                         div(style='height:800px; overflow-y: scroll', tableOutput('table'))
+                       )
+              ),
+              
+              
+              tabPanel(title = "Submission",
+                    
+                       sidebarPanel(
+                         helpText("Use the text box to write information that may be missing in the database."),
+                         br(),
+                         helpText("Click the 'Submit' button below to submit your entry for review."),
+                         actionButton("submitEntry", "Submit"),
+                         height = "1000px"
+                      ),
+                       mainPanel(
+                         textAreaInput("entryText", "", "Data Summary", width = "500px", height = "300px"),
+                         verbatimTextOutput("value")
+                       )
+                  
+              ),
+              
+              tabPanel(title = "Removal",
+                       sidebarPanel(
+                         helpText("Use the text box to write information that may be missing in the database."),
+                         br(),
+                         helpText("Click the 'Submit' button below to submit your removal request for review."),
+                         actionButton("submitRemoval", "Submit")
+                         
+                       ),
+                       mainPanel(
+                         textAreaInput("removalText", "", "Data Summary", width = "500px", height = "300px")
+                       )
+                       
+              ),
+              
+              tabPanel(title = "ADMIN",
+                  #use shiny js to disable the ID field
+                  shinyjs::useShinyjs(),
+                  #data table
+                  DT::dataTableOutput("responses", width = 300), 
+                  #input fields
+                  tags$hr(),
+                  shinyjs::disabled(textInput("id", "Id", "0")),
+                  textInput("chrom", "Chromosome", ""),
+                  textInput("chromStart", "ChromStart", ""),
+                  textInput("chromEnd", "ChromEnd", ""),
+                  textInput("name", "Name", ""),
+                  textInput("score", "Score", ""),
+                  textInput("blocks", "Blocks", ""),
+                  #action buttons
+                  actionButton("submit", "Submit"),
+                  actionButton("new", "New"),
+                  actionButton("delete", "Delete")
+              )
+              
+  )          
+  
+  
 )
 
-server <- function(input, output, session) {
+
+
+
+server <- function(input, output, session){
+  
+  #output$dataset <- renderText(input$dataset)
+  #output$option2 <- renderText(input$option2)
+  
+  #--fetch from options user selected--#
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "type 1" = data[1:100,],
+           "type 2" = data[100:200,],
+           "type 3" = data[200:300,])
+  })
+  
+  #---show selected data in main panel---#
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  #---fetch from options for download---#
+  fileext <- reactive({
+    switch(input$type,
+           "Excel (CSV)" = "csv", 
+           "Text(TSV)" = "txt", 
+           "Text(Space Separated)" = "txt", 
+           "Doc" = "doc"
+    )
+  })
+  
+  output$value <-renderText({input$caption})
+  
+  randomVals <- eventReactive(input$submitEntry, {
+    runif(input$entryText)
+  })
+  
+  randomVals <- eventReactive(input$submitRemoval, {
+    runif(input$removalText)
+  })
+  
+  
+  
+  #____________ADMIN_____________#
+  
   
   # input fields are treated as a group
   formData <- reactive({
@@ -68,7 +199,12 @@ server <- function(input, output, session) {
   colnames = unname(GetTableMetadata()$fields)[-1]
   )     
   
+  
+  
 }
+
+
+
 
 ##### HELPERS #####
 
@@ -148,4 +284,7 @@ GetTableMetadata <- function() {
   return (result)
 }
 
+
 shinyApp(ui = ui, server = server)
+
+
