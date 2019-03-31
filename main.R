92ea65894c132ff87b432889253b3c57b3a95575
amy0012 <35613028+amy0012@users.noreply.github.com>
2019-03-31 10:54:38 -0500

    Update main.R

--- main.R
+++ main.R
@@ -1,21 +1,33 @@
 #
 # This is a Shiny web application. 
-# 1st Prototype for the Biology Tool Kit
-# 2/3/2019
+# 3rd Prototype for the Biology Tool Kit
+# 3/31/2019
 #
 
 library(shiny)
 library(shinythemes)
 library(DT)
+library(gmailr)
+library(dplyr)
 
-data <- df
-names(data) <- c('chrom', 'start', 'stop', 'name', 'score', 'strand')
+cellLine <- wgEncodeRegTfbsClusteredWithCellsV3.bed
+cellLine <- cellLine[1:10000,]
+names(cellLine) <- c('chrom', 'start', 'stop', 'name', 'score', 'strand')
 
-ui <- navbarPage(
+traits <- RESULTS
+
+traitOptions <- unique(c(as.character(traits$Trait)))
+meshOptions <- unique(c(as.character(traits$MESH.CATEGORY)))
+
+#------------------------- USER INTERFACE ----------------------------#
+
+ui <- fluidPage(
+  #tags$img(src = "headerwhite.png", height = 250, width = 1500),
+  navbarPage(
   
   "Computational Biology Bio-Informatics Toolkit",
- theme = shinytheme("cosmo"),
-
+  theme = shinytheme("flatly"),
+  ## yeti, cerulean, cosmo
   #---page tab settings---#
   tabsetPanel(type = "tab", position = "right",
               
@@ -23,7 +35,8 @@ ui <- navbarPage(
               tabPanel(title = "Search", 
                        sidebarPanel(
                          
-                         condition = "input$id == 'Search'",
+                         
+
                          helpText("Welcome to the Biology Bio-Information ToolKit!
                                   Use the options below to search."),
                          br(),
@@ -32,8 +45,8 @@ ui <- navbarPage(
                          # textInput("option1", "select option1"),
                          # textInput("option2", "select option2"),
                          
-                         selectInput("dataset", "Select a disease type: ", c("", "type 1", "type 2", "type 3")),
-                         selectInput("option2", "Select a tissue and cell line: ", c("", "cell line 1", "cell line 2", "cell line 3", "cell line 4")),
+                         selectInput("dataset", "Select a trait: ", traitOptions),
+                         selectInput("meshData", "Select a mesh trait category: ", meshOptions),
                          
                          helpText("Select your download format."),
                          radioButtons("type", "Format Type: ", 
@@ -47,25 +60,25 @@ ui <- navbarPage(
                          ),
                        mainPanel(
                          h3("Search Results: "),
-                         div(style='height:800px; overflow-y: scroll', tableOutput('table'))
+                         div(style='height:800px; width: 570px; overflow-y: scroll', tableOutput('table'))
                        )
               ),
               
               
               tabPanel(title = "Submission",
-                    
+                       
                        sidebarPanel(
                          helpText("Use the text box to write information that may be missing in the database."),
                          br(),
                          helpText("Click the 'Submit' button below to submit your entry for review."),
                          actionButton("submitEntry", "Submit"),
                          height = "1000px"
-                      ),
+                       ),
                        mainPanel(
                          textAreaInput("entryText", "", "Data Summary", width = "500px", height = "300px"),
                          verbatimTextOutput("value")
                        )
-                  
+                       
               ),
               
               tabPanel(title = "Removal",
@@ -83,49 +96,56 @@ ui <- navbarPage(
               ),
               
               tabPanel(title = "ADMIN",
-                  #use shiny js to disable the ID field
-                  shinyjs::useShinyjs(),
-                  #data table
-                  DT::dataTableOutput("responses", width = 300), 
-                  #input fields
-                  tags$hr(),
-                  shinyjs::disabled(textInput("id", "Id", "0")),
-                  textInput("chrom", "Chromosome", ""),
-                  textInput("chromStart", "ChromStart", ""),
-                  textInput("chromEnd", "ChromEnd", ""),
-                  textInput("name", "Name", ""),
-                  textInput("score", "Score", ""),
-                  textInput("blocks", "Blocks", ""),
-                  #action buttons
-                  actionButton("submit", "Submit"),
-                  actionButton("new", "New"),
-                  actionButton("delete", "Delete")
+                       #use shiny js to disable the ID field
+                       shinyjs::useShinyjs(),
+                       #data table
+                       DT::dataTableOutput("responses", width = 300), 
+                       #input fields
+                       tags$hr(),
+                       shinyjs::disabled(textInput("id", "Id", "0")),
+                       textInput("chrom", "Chromosome", ""),
+                       textInput("chromStart", "ChromStart", ""),
+                       textInput("chromEnd", "ChromEnd", ""),
+                       textInput("name", "Name", ""),
+                       textInput("score", "Score", ""),
+                       textInput("blocks", "Blocks", ""),
+                       #action buttons
+                       actionButton("submit", "Submit"),
+                       actionButton("new", "New"),
+                       actionButton("delete", "Delete")
               )
               
   )          
   
   
 )
-
+)
 
 
 
 server <- function(input, output, session){
   
-  #output$dataset <- renderText(input$dataset)
-  #output$option2 <- renderText(input$option2)
-  
-  #--fetch from options user selected--#
+  #--fetch from trait option selected--#
   datasetInput <- reactive({
     switch(input$dataset,
-           "type 1" = data[1:100,],
-           "type 2" = data[100:200,],
-           "type 3" = data[200:300,])
+           "Waist Circumference" = cellLine[cellLine$chrom == "chrX",],
+           "Glucose" = cellLine[cellLine$score < 180,],
+           "Forced Expiratory Volume" = cellLine[cellLine$strand == "H1-hESC",],
+           "Intuition" = cellLine[cellLine$score >= 1000,])
+  })
+  
+  #--fetch from mesh category option selected--#  
+  meshInput <- reactive({
+    switch(input$meshData,
+           "Nutritional and Metabolic Diseases" = cellLine[cellLine$chrom == "chrX",],
+           "type 2" = cellLine[cellLine$score < 180,],
+           "type 3" = cellLine[cellLine$strand == "chrY",])
   })
   
   #---show selected data in main panel---#
   output$table <- renderTable({
     datasetInput()
+    #meshInput()
   })
   
   #---fetch from options for download---#
@@ -140,9 +160,19 @@ server <- function(input, output, session){
   
   output$value <-renderText({input$caption})
   
-  randomVals <- eventReactive(input$submitEntry, {
-    runif(input$entryText)
-  })
+  emails <- eventReactive(input$submitEntry
+                          , {
+                            test_email <- mime() %>%
+                              to(c("ansleigh.yancey@gmail.com")) %>%
+                              html_body(input$entryText) %>%
+                              subject("subject")
+                            # attach_file("file.txt")
+                            
+                            test_email <- sub("Content-Disposition: inline\r\n--","Content-Disposition: inline\r\n\r\n--", as.character(test_email))
+                            
+                            ret_val <- send_message(test_email)
+                          })                  
+
   
   randomVals <- eventReactive(input$submitRemoval, {
     runif(input$removalText)
@@ -182,8 +212,8 @@ server <- function(input, output, session){
   # Select row in table -> show details in inputs
   observeEvent(input$responses_rows_selected, {
     if (length(input$responses_rows_selected) > 0) {
-      data <- ReadData()[input$responses_rows_selected, ]
-      UpdateInputs(data, session)
+      cellLine <- ReadData()[input$responses_rows_selected, ]
+      UpdateInputs(cellLine, session)
     }
     
   })
@@ -208,15 +238,15 @@ server <- function(input, output, session){
 
 ##### HELPERS #####
 
-CastData <- function(data) {
-  datar <- data.frame(chrom = data["chrom"], 
-                      chromStart = as.integer(data["chromStart"]), 
-                      chromEnd = as.integer(data["chromEnd"]),
-                      name = data["name"],
-                      score = as.integer(data["score"]),
-                      blocks = data["blocks"])
+CastData <- function(cellLine) {
+  datar <- data.frame(chrom = cellLine["chrom"], 
+                      chromStart = as.integer(cellLine["chromStart"]), 
+                      chromEnd = as.integer(cellLine["chromEnd"]),
+                      name = cellLine["name"],
+                      score = as.integer(cellLine["score"]),
+                      blocks = cellLine["blocks"])
   
-  rownames(datar) <- data["id"]
+  rownames(datar) <- cellLine["id"]
   return (datar)
 }
 
@@ -226,13 +256,13 @@ CreateDefaultRecord <- function() {
   return (mydefault)
 }
 
-UpdateInputs <- function(data, session) {
-  updateTextInput(session, "id", value = unname(rownames(data)))
-  updateTextInput(session, "chrom", value = unname(data["chrom"]))
-  updateTextInput(session, "chromStart", value = as.integer(data["chromStart"]))
-  updateTextInput(session, "chromEnd", value = as.integer(data["chromEnd"]))
-  updateTextInput(session, "score", value = as.integer(data["score"]))
-  updateTextInput(session, "blocks", value = as.integer(data["blocks"]))
+UpdateInputs <- function(cellLine, session) {
+  updateTextInput(session, "id", value = unname(rownames(cellLine)))
+  updateTextInput(session, "chrom", value = unname(cellLine["chrom"]))
+  updateTextInput(session, "chromStart", value = as.integer(cellLine["chromStart"]))
+  updateTextInput(session, "chromEnd", value = as.integer(cellLine["chromEnd"]))
+  updateTextInput(session, "score", value = as.integer(cellLine["score"]))
+  updateTextInput(session, "blocks", value = as.integer(cellLine["blocks"]))
 }
 
 GetNextId <- function() {
@@ -245,14 +275,14 @@ GetNextId <- function() {
 
 ##### CRUD Methods #####
 
-CreateData <- function(data) {
+CreateData <- function(cellLine) {
   
-  data <- CastData(data)
-  rownames(data) <- GetNextId()
+  cellLine <- CastData(cellLine)
+  rownames(cellLine) <- GetNextId()
   if (exists("responses")) {
-    responses <<- rbind(responses, data)
+    responses <<- rbind(responses, cellLine)
   } else {
-    responses <<- data
+    responses <<- cellLine
   }
 }
 
@@ -262,13 +292,13 @@ ReadData <- function() {
   }
 }
 
-UpdateData <- function(data) {
-  data <- CastData(data)
-  responses[row.names(responses) == row.names(data), ] <<- data
+UpdateData <- function(cellLine) {
+  cellLine <- CastData(cellLine)
+  responses[row.names(responses) == row.names(cellLine), ] <<- cellLine
 }
 
-DeleteData <- function(data) {
-  responses <<- responses[row.names(responses) != unname(data["id"]), ]
+DeleteData <- function(cellLine) {
+  responses <<- responses[row.names(responses) != unname(cellLine["id"]), ]
 }
 
 GetTableMetadata <- function() {
